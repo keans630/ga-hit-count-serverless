c3baccff27cb754846c4a503d7ccb41cd827cd76
keans630 <128244243+keans630@users.noreply.github.com>
2023-04-23 18:07:00 +0800

    Update config.ts

--- api/config.ts
+++ api/config.ts
@@ -1,7 +1,7 @@
 /**
  * Google Analytics query configurations
  *
- **
+ *
  * ! If you are deploying this with your own account
  * ! , then you will need to change this config file.
  * ! Don't put your privateKey inside this file directly!
